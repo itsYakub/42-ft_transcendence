eea3f4f628891adca37ac27f7228a8282bdd3eca
Luke Willis <lwillis@c2r8s3.42warsaw.pl>
2025-07-09 10:11:44 +0200

    update

--- src/frontend/public/js/register.js
+++ src/frontend/public/js/register.js
@@ -1,33 +1,9 @@
-function validEmail(email) {
-    return true;
-}
-function checkNick(nick) {
-    return nick.length > 0 ? "" : "Please provide a nickname";
-}
-function checkEmail(email) {
-    return validEmail(email) ? "" : "Invalid email";
-}
-function checkPassword(password) {
-    return password.length > 7 ? "" : "Password needs to be 8+ characters";
-}
 export function setupRegisterForm() {
-    document.getElementById("signupButton").addEventListener("click", async () => {
-        const nick = document.getElementById("nickBox").value;
-        const email = document.getElementById("emailBox").value;
-        const password = document.getElementById("passwordBox").value;
-        const nickError = checkNick(nick);
-        const emailError = checkEmail(nick);
-        const passwordError = checkPassword(nick);
-        document.getElementById("nickError").textContent = nickError;
-        document.getElementById("emailError").textContent = emailError;
-        document.getElementById("passwordError").textContent = passwordError;
-        if (0 == nickError.length + emailError.length + passwordError.length) {
-            const response = await fetch("/register", {
-                method: "POST",
-                body: JSON.stringify({
-                    nick, email, password
-                })
-            });
-        }
+    const form = document.getElementById("registerForm");
+    form.addEventListener("submit", async (e) => {
+        e.preventDefault();
+        const nick = form.nick.value;
+        const email = form.email.value;
+        const password = form.password.value;
     });
 }
--- src/frontend/public/js/register.ts
+++ src/frontend/public/js/register.ts
@@ -1,47 +1,18 @@
-function validEmail(email) {
-	return true;//(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email));
-}
-
-function checkNick(nick: string): string {
-	return nick.length > 0 ? "" : "Please provide a nickname";
-}
-
-function checkEmail(email: string): string {
-	return validEmail(email) ? "" : "Invalid email";
-}
-
-function checkPassword(password: string): string {
-	return password.length > 7 ? "" : "Password needs to be 8+ characters";
-}
-
 export function setupRegisterForm() {
-	document.getElementById("signupButton").addEventListener("click", async () => {
-		const nick = (<HTMLInputElement>document.getElementById("nickBox")).value;
-		const email = (<HTMLInputElement>document.getElementById("emailBox")).value;
-		const password = (<HTMLInputElement>document.getElementById("passwordBox")).value;
-		const nickError = checkNick(nick);
-		const emailError = checkEmail(nick);
-		const passwordError = checkPassword(nick);
-		document.getElementById("nickError").textContent = nickError;
-		document.getElementById("emailError").textContent = emailError;
-		document.getElementById("passwordError").textContent = passwordError;
+	const form = <HTMLFormElement>document.getElementById("registerForm");
+	form.addEventListener("submit", async (e) => {
+		e.preventDefault();
+		const nick = form.nick.value;
+		const email = form.email.value;
+		const password = form.password.value;
 
-		// Frontend okay, now to check with the db
-		if (0 == nickError.length + emailError.length + passwordError.length) {
-			const response = await fetch("/register", {
-				method: "POST",
-				body: JSON.stringify({
-					nick, email, password
-				})
-			});
-		}
+		const response = await fetch("/register", {
+			method: "POST",
+			body: JSON.stringify({
+				nick, email, password
+			})
+		});
 
-		// const errors = {
-		// 	nick: checkNick(nick),
-		// 	email: checkEmail(email),
-		// 	password: checkPassword(password)
-		// }
-		// console.log(errors);
 		// const response = await fetch("/register", {
 		// 	method: "POST",
 		// 	body: JSON.stringify({
