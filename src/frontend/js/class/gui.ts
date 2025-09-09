d267c0cdbeae433976de242c6c0bbbf0ebdb5078
Jakub Oleksiak <yakubofficialmail@gmail.com>
2025-09-09 23:20:40 +0200

    [ joleksia ] GUI WIP
    I've found a potential good positioning for a simple GUI for the game

--- src/frontend/js/class/gui.ts
+++ src/frontend/js/class/gui.ts
@@ -23,18 +23,23 @@ export class Gui {
 		this.m_player0 = p0;
 		this.m_player1 = p1;
 		
-		var mesh = BABYLON.MeshBuilder.CreatePlane('ui-plane0', { width: 4, height: 1 } ); 
-		var advancedTexture = GUI.AdvancedDynamicTexture.CreateForMesh(mesh);
+		var mesh0 = BABYLON.MeshBuilder.CreatePlane('ui-plane0', { size: 2, width: 4, height: 4 } ); 
+		var advancedTexture0 = GUI.AdvancedDynamicTexture.CreateForMesh(mesh0);
+		mesh0.rotation = new BABYLON.Vector3(1.5, 0.0, 0.0);
+		mesh0.position = new BABYLON.Vector3(-5.5, 2.5, 2.5);
 
 		var rect0 = new GUI.Rectangle();
 		rect0.cornerRadius = 8;
-		rect0.background = 'rgb(0.74, 0.17, 0.83)';
-		advancedTexture.addControl(rect0);
+		rect0.background = '#00000000';
+		rect0.color = '#00000000';
+		advancedTexture0.addControl(rect0);
 
 		var text0 = new GUI.TextBlock();
 		text0.color = 'white';
-		text0.fontSize = 32;
-		text0.text = p0.nick;
+		text0.fontFamily = 'Sans-serif';
+		text0.fontSize = 96;
+		text0.fontWeight = 'bolder';
+		text0.text = p0.nick + ': ' + p0.score;
 		rect0.addControl(text0);
 	}
 
